7f403d61006a49992bf6608660ca930c9515a4ae
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2017-02-01 13:34:54 -0800

    Update test_bessel_functions.py

--- test/test_bessel_functions.py
+++ test/test_bessel_functions.py
@@ -16,7 +16,7 @@ sys.path.insert(0, myPath + '/../')
 from sound_field_analysis import sph
 
 
-"""def test_sph_harm():
+def test_sph_harm():
     assert sph.sph_harm(0, 0, 0, 0, type='complex') == approx(0.282094791773878)
     assert sph.sph_harm(0, 1, 0.1, 0.1, type='complex') == approx(0.486161534508712)
     assert sph.sph_harm(-2, 2, -0.1, -0.1, type='complex') == approx(0.003773142018527 + 0.000764853752555j)
@@ -24,7 +24,7 @@ from sound_field_analysis import sph
     assert sph.sph_harm(17, 17, 7, 6, type='complex') == approx(2.202722537106286e-10 - 8.811259608537731e-11j)
     assert sph.sph_harm(-17, 17, 6, -7, type='complex') == approx(4.945682459644879e-05 - 4.843297071701738e-04j)
 
-
+"""
 def test_besselj():
     n, k = generate_n_k()
     results = np.array([[-0.680560185349146j, 0.557936507910100, 0.511827671735918, -0.557936507910100, -0.387142217276067j],
